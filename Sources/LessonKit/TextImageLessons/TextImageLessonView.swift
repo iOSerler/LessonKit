9592c48f306f3b37b029ab8324b5cc15a3c9b734
AnnaDluzhinskaya <a.dluzhinskaya@gmail.com>
2022-07-01 13:57:50 +0300

    update text lesson 2.0

--- Sources/LessonKit/TextImageLessons/TextImageLessonView.swift
+++ Sources/LessonKit/TextImageLessons/TextImageLessonView.swift
@@ -30,6 +30,7 @@ public struct TextImageLessonView: View {
                     .font(.custom(settings.descriptionFont, size: 14))
                     .foregroundColor(Color(settings.descriptionTextColor))
                     .multilineTextAlignment(.leading)
+                    .padding(.horizontal, 20)
                 HStack {
                     Image(settings.durationOffIcon)
                     
